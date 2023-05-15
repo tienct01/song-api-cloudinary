94255a68ee9f38b77fa8dc1bf3e0940177bced3e
Đoàn Tiến <doanvanduytien@gmail.com>
2023-05-15 23:38:24 +0700

    update server url

--- index.js
+++ index.js
@@ -23,6 +23,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static('./uploads'));
 
+swaggerDocs.servers.shift({
+	url: process.env.SERVER_URL,
+	description: 'API',
+});
 // swagger ui
 app.use('/apis', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 // song api

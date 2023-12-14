bce975bf2e5187a49390ed4708b7aa065bf67e7a
SBC-TienDVD <tiendvd@bsscommerce.com>
2023-12-14 23:02:34 +0700

    fix check uploads folder

--- middlewares/multer.js
+++ middlewares/multer.js
@@ -9,8 +9,8 @@ const diskStorage = multer.diskStorage({
 		cb(null, file.fieldname + '-' + uniqueSuffix);
 	},
 	destination: (req, file, cb) => {
-		if(!fs.existsSync(path.join(__dirname) + '/uploads')) {
-			fs.mkdirSync(path.join(__dirname), 'uploads');
+		if(!fs.existsSync(path.join(__dirname, 'uploads'))) {
+			fs.mkdirSync(path.join(__dirname, 'uploads'));
 		}
 		cb(null, 'uploads');
 	},
